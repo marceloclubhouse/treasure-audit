65f81dac394d1b7d4d825ed7d4203f843b023a0d
Marcelo <marceloclubhouse@gmail.com>
2021-09-19 18:20:57 -0700

    Added package script as fallback for spec file

---
+++ package.sh
@@ -0,0 +1,15 @@
+pyinstaller --onefile --noconsole --name "Treasure Audit" --icon "resources/icon_logo.icns" \
+--add-data "resources/icon_logo.ico:resources" \
+--add-data "resources/icon_logo.icns:resources" \
+--add-data "resources/icon_logo.png:resources" \
+--add-data "feather/upload.svg:feather" \
+--add-data "feather/x-octagon.svg:feather" \
+--add-data "feather/layers.svg:feather" \
+--add-data "feather/external-link.svg:feather" \
+--add-data "feather/x.svg:feather" \
+--add-data "feather/code.svg:feather" \
+--add-data "feather/monitor.svg:feather" \
+--add-data "feather/edit-3.svg:feather" \
+--add-data "feather/info.svg:feather" \
+--add-data "feather/users.svg:feather" \
+run.py
